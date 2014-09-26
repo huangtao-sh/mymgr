c26f89809c8859f3b8f4483b59e7aa03d6d6412b
huangtao-sh <huangtao.jh@gmail.com>
2014-09-26 21:57:25 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from distutils.core import setup
+setup(
+        name='mymgr',
+        version='0.0.1',
+        author='Huang tao',
+        author_email='huangtao.jh@gmail.com',
+        platforms='any',
+        description='mysql connection lib',
+        long_description='mysql connection lib',
+        packages=['mymgr'],
+        license='GPL',
+        )
