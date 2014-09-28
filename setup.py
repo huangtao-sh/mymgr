a9d02016a242d61bc03b91848c10cebc8c998193
huangtao-sh <huangtao.jh@gmail.com>
2014-09-28 10:49:03 +0800

    modified setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from distutils.core import setup
+setup(
+        name='mymgr',
+        version='0.0.1',
+        author='Huang tao',
+        author_email='huangtao.jh@gmail.com',
+        platforms='any',
+        description='mymgr',
+        long_description='mymgr',
+        packages=['mymgr'],
+        license='GPL',
+        requires=['']
+        )
